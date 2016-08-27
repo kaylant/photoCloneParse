ac0ef90f03224ea8067a08cec036738fbb6933c0
kaylant <kaylant@gmail.com>
2016-08-26 20:57:13 -0500

    image post feed set up as table view controller

---
+++ ParseStarterProject/cell.swift
@@ -0,0 +1,19 @@
+//
+//  cell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Kaylan Smith on 8/26/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class cell: UITableViewCell {
+
+    @IBOutlet var postedImage: UIImageView!
+
+    @IBOutlet var userName: UILabel!
+
+    @IBOutlet var message: UILabel!
+    
+}
